c7d07edade8aed60766af46edb4d2203e3e9ac5b
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-21 23:41:54 -0500

    migrate to androidx and kotlin

---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.kt
@@ -0,0 +1,16 @@
+package co.com.ceiba.mobile.pruebadeingreso.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.com.ceiba.mobile.pruebadeingreso.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/PostActivity.kt
@@ -0,0 +1,16 @@
+package co.com.ceiba.mobile.pruebadeingreso.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.com.ceiba.mobile.pruebadeingreso.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
