14eb1e7636870e5426172825959678450971d1f3
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-22 00:17:28 -0500

    modify instance of xml layout to binding

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.kt
@@ -3,11 +3,15 @@ package co.com.ceiba.mobile.pruebadeingreso.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.com.ceiba.mobile.pruebadeingreso.R
+import co.com.ceiba.mobile.pruebadeingreso.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindig: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindig = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindig.root)
     }
 
     override fun onStart() {
