5187de1484018283ea2592e288214c0de390048b
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-22 12:56:34 -0500

    create model user and post

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/local/UserDao.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/local/UserDao.kt
@@ -1,4 +1,16 @@
 package co.com.ceiba.mobile.pruebadeingreso.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import co.com.ceiba.mobile.pruebadeingreso.data.model.UserEntity
+
+@Dao
 interface UserDao {
+    @Query("SELECT * FROM UserEntity")
+    suspend fun getAllUsers(): List<UserEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveUsers(user: UserEntity)
 }
--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/model/Post.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/model/Post.kt
@@ -1,2 +1,8 @@
 package co.com.ceiba.mobile.pruebadeingreso.data.model
 
+data class Post(
+    val userId: Int = -1,
+    val id: Int = -1,
+    val title: String = "",
+    val body: String = "",
+)
