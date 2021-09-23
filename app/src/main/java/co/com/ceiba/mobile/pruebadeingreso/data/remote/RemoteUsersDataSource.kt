70d5d3c9ffef8ee14d9f7d963064b776afca9cfc
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-23 17:09:36 -0500

    create method getpost, recive one parameter type int and return list

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/remote/RemoteUsersDataSource.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/data/remote/RemoteUsersDataSource.kt
@@ -1,8 +1,11 @@
 package co.com.ceiba.mobile.pruebadeingreso.data.remote
 
-import co.com.ceiba.mobile.pruebadeingreso.data.model.UsersList
+import co.com.ceiba.mobile.pruebadeingreso.data.model.Post
+import co.com.ceiba.mobile.pruebadeingreso.data.model.User
 import co.com.ceiba.mobile.pruebadeingreso.repository.WebService
 
 class RemoteUsersDataSource(private val webService: WebService) {
-    suspend fun getUsers(): UsersList = webService.getUsers()
+    suspend fun getUsers(): List<User> = webService.getUsers()
+
+    suspend fun getPost(id: Int): List<Post> = webService.getPost(id)
 }
