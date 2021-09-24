12b56ea133d5a39ea5907d1a6ff3efb8b8c42f2b
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-24 10:09:45 -0500

    in this file declare provides and singleton instances for use in inject of activity

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/utils/UsersModule.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/utils/UsersModule.kt
@@ -1,4 +1,26 @@
 package co.com.ceiba.mobile.pruebadeingreso.view.utils
 
-class UsersModule {
+import android.content.Context
+import co.com.ceiba.mobile.pruebadeingreso.data.local.AppDataBase
+import co.com.ceiba.mobile.pruebadeingreso.data.local.LocalUsersDataSource
+import co.com.ceiba.mobile.pruebadeingreso.data.remote.RemoteUsersDataSource
+import co.com.ceiba.mobile.pruebadeingreso.presentation.UserViewModelFactory
+import co.com.ceiba.mobile.pruebadeingreso.repository.RetrofitClient
+import co.com.ceiba.mobile.pruebadeingreso.repository.UserRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class UsersModule(private val context: Context) {
+    @Provides
+    @Singleton
+    fun provideUsersViewModelFactory() = UserViewModelFactory(provideRepositoryUserImpl())
+
+    @Provides
+    @Singleton
+    fun provideRepositoryUserImpl() = UserRepositoryImpl(
+        RemoteUsersDataSource(RetrofitClient.webService),
+        LocalUsersDataSource(AppDataBase.getDatabase(context).userDao())
+    )
 }
