1fa26188aa33b4e16a3ca6e1ae072c9a8ca032ab
Cristian Castellanos <cristiancastellanos@Mac-mini-de-Cristian.local>
2021-09-24 10:08:08 -0500

    creat this interface, user for dagger, in this file declare activity at inject

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/utils/UsersComponent.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/utils/UsersComponent.kt
@@ -1,4 +1,13 @@
 package co.com.ceiba.mobile.pruebadeingreso.view.utils
 
+import co.com.ceiba.mobile.pruebadeingreso.view.MainActivity
+import co.com.ceiba.mobile.pruebadeingreso.view.PostActivity
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [UsersModule::class])
 interface UsersComponent {
+    fun inject(mainActivity: MainActivity)
+    fun inject(postActivity: PostActivity)
 }
